b0cee1073c682f90b9ac64346a78e4e40c366739
Gabriel Lins <gabriellf.glf@gmail.com>
2022-09-08 23:29:07 -0300

    questão1

---
+++ questão1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+    int vet_aleatorio[30], vet_par[30], vet_impar[30];
+
+    for (int i = 0; i < 30; i++)
+    {
+        vet_aleatorio[i] = rand() % 100;
+    }
+
+    for (int i = 0; i < 30; i++)
+    {
+        printf("%d\n", vet_aleatorio[i]);
+    }
+
+    printf("---------------------------\n");
+
+    for (int i = 0; i < 30; i++)
+    {
+        if (vet_aleatorio[i] % 2 == 0)
+        {
+            vet_par[i] = vet_aleatorio[i];
+            printf("PAR: %d\n",vet_par[i]);
+        }   
+    }
+
+    for (int i = 0; i < 30; i++)
+    {
+        if (vet_aleatorio[i] % 2 != 0)
+        {
+            vet_impar[i] = vet_aleatorio[i];
+            printf("IMPAR: %d\n",vet_impar[i]);
+        }  
+    }
+
+    return 0;
+}
