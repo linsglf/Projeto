1da835e3e3605ce088e31b711b7a618647c6d342
Gabriel Lins <gabriellf.glf@gmail.com>
2022-09-09 19:38:05 -0300

    Update questão2.c

--- questão2.c
+++ questão2.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-float calculo_imc(){
+int calculo_imc(){
 
-    float peso, altura;
+    int peso, altura_cm;
+    float altura_m, altura_quadrado;
 
     printf("Digite seu peso em Kg: ");
-    scanf("%f", &peso);
-    printf("Digite sua altura em metros: ");
-    scanf("%f", &altura);
+    scanf("%d", &peso);
+    printf("Digite sua altura em cm: ");
+    scanf("%d", &altura_cm);
 
-    float imc = peso / (altura * altura);
+    altura_m = (float)altura_cm / 100;
 
-    return imc;
+
+    altura_quadrado = altura_m * altura_m;
+
+    float imc = round((float)peso / (float)altura_quadrado);
+
+    int imc_int = imc;
+
+    return imc_int;
 }
 
 int main()
@@ -24,48 +33,48 @@ int main()
 
     if (opcao == 1){
 
-      float imc = calculo_imc();
+      int imc = calculo_imc();
       int idade;
 
       printf("Qual a sua idade: ");
       scanf("%d", &idade);
 
-      float gordura_masculina = (imc * 1.2) + (idade * 0.23) - 16.2; 
+      int gordura_masculina = (imc * 1.2) + (idade * 0.23) - 16.2; 
 
-      printf("Seu imc: %.2f\n", imc);
-      printf("Seu percentual de gordura: %.2f\n", gordura_masculina);
+      printf("Seu imc: %d\n", imc);
+      printf("Seu percentual de gordura: %d\n", gordura_masculina);
 
       if (gordura_masculina < 11){
             printf("Atleta!");
       }else if (gordura_masculina >= 11 && gordura_masculina <= 13){
             printf("Bom!");
-      }else if (gordura_masculina > 13 && gordura_masculina <= 20){
+      }else if (gordura_masculina >= 14 && gordura_masculina <= 20){
             printf("Normal!");
-      }else if (gordura_masculina > 20 && gordura_masculina <= 23){
+      }else if (gordura_masculina >= 21 && gordura_masculina <= 23){
             printf("Elevado!");
       }else if (gordura_masculina > 23){
             printf("Alto!");
       }
     }else if (opcao == 2){
 
-      float imc = calculo_imc();
+      int imc = calculo_imc();
       int idade;
 
       printf("Qual a sua idade: ");
       scanf("%d", &idade);
 
-      float gordura_feminina = (imc * 1.2) + (idade * 0.23) - 5.4; 
+      int gordura_feminina = (imc * 1.2) + (idade * 0.23) - 5.4; 
 
-      printf("Seu imc: %.2f\n", imc);
-      printf("Seu percentual de gordura: %.2f\n", gordura_feminina);
+      printf("Seu imc: %d\n", imc);
+      printf("Seu percentual de gordura: %d\n", gordura_feminina);
 
       if (gordura_feminina < 16){
             printf("Atleta!");
       }else if (gordura_feminina >= 16 && gordura_feminina <= 19){
             printf("Bom!");
-      }else if (gordura_feminina > 19 && gordura_feminina <= 28){
+      }else if (gordura_feminina >= 20 && gordura_feminina <= 28){
             printf("Normal!");
-      }else if (gordura_feminina > 28 && gordura_feminina <= 31){
+      }else if (gordura_feminina >= 29 && gordura_feminina <= 31){
             printf("Elevado!");
       }else if (gordura_feminina > 31){
             printf("Alto!");
