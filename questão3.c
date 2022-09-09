b01ebf58964ae26714866c87cec7de4f2ccfbf77
Gabriel Lins <gabriellf.glf@gmail.com>
2022-09-09 15:00:22 -0300

    questão 3 completa
    questão 3 finalizada.

--- questão3.c
+++ questão3.c
@@ -41,14 +41,64 @@ float aritimetico (){
 }
 
 float ponderado(){
+     
+    int qtd_notas;
+    float teste = 0;
+
+    printf("Digite a quantidade de notas a serem calculadas: ");
+    scanf("%d", &qtd_notas);
+    fflush(stdin);
+
+    int vet_peso[qtd_notas];
     
+    for (int i = 0; i < qtd_notas; i++){
+        printf("Digite a nota %d: ", i + 1);
+        scanf("%f", &notas[i]);
+        fflush(stdin);
+        printf("Digite o peso da nota %d: ", i + 1);
+        scanf("%d", &vet_peso[i]);
+        fflush(stdin);
+    }
+
+    float soma_peso = 0;
+
+    for (int i = 0; i < qtd_notas; i++){
+        soma_peso = soma_peso + (float)vet_peso[i];
+    }
+
+    float soma = 0;
+    
+    for (int i = 0; i < qtd_notas; i++)
+    {
+        soma = soma + (notas[i] * (float)vet_peso[i]);
+    }
+
+    float media = soma / soma_peso;
+
+    for (int i = 0; i < qtd_notas; i++){
+        if (notas[i] > 10 || notas[i] < 0){
+            printf("Nota %d invalida!\n", i + 1);
+        }        
+    }
+
+    for (int i = 0; i < qtd_notas; i++){
+        if (notas[i] > 10 || notas[i] < 0){
+            return 0;
+        }else{
+            return media; 
+        }
+    }
+
+    printf("%.2f", media);
+    
+    return 0;
 }
 
 int main()
 {   
     int opcao;
 
-    printf("1 - Media aritimetica\n2 - Media ponderada\nDigite sua escolha:");
+    printf("1 - Media aritimetica\n2 - Media ponderada\nDigite sua escolha: ");
     scanf("%d", &opcao);
     fflush(stdin);
 
@@ -68,10 +118,9 @@ int main()
         if (media_ponderada == 0){
             printf("Tente novamente!");
         }else{
-            printf("Sua media aritimetica: %.2f", media_ponderada);
+            printf("Sua media ponderada: %.2f", media_ponderada);
         }
     }
-    
     
     return 0;
 }
