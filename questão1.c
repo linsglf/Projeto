ab16dc13278a2c7ab35573202b495ce7320af6eb
Gabriel Lins <gabriellf.glf@gmail.com>
2022-09-09 17:36:53 -0300

    Projeto finalizado!
    Todas questões do projeto resolvidas.

--- questão1.c
+++ questão1.c
@@ -5,24 +5,19 @@ int main()
 {   
     int vet_aleatorio[30], vet_par[30], vet_impar[30];
 
-    for (int i = 0; i < 30; i++)
-    {
+    for (int i = 0; i < 30; i++){
         vet_aleatorio[i] = rand() % 100;
     }
 
-    for (int i = 0; i < 30; i++)
-    {
-        if (vet_aleatorio[i] % 2 == 0)
-        {
+    for (int i = 0; i < 30; i++){
+        if (vet_aleatorio[i] % 2 == 0){
             vet_par[i] = vet_aleatorio[i];
             printf("PAR: %d\n",vet_par[i]);
         }   
     }
 
-    for (int i = 0; i < 30; i++)
-    {
-        if (vet_aleatorio[i] % 2 != 0)
-        {
+    for (int i = 0; i < 30; i++){
+        if (vet_aleatorio[i] % 2 != 0){
             vet_impar[i] = vet_aleatorio[i];
             printf("IMPAR: %d\n",vet_impar[i]);
         }  
