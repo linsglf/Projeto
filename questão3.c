a4995b1a8c017b503c39c65c03a2ae40bc3ac591
Gabriel Lins <gabriellf.glf@gmail.com>
2022-09-09 01:17:15 -0300

    questão 3 - 50%
    ainda resta fazer a média ponderada.

--- questão3.c
+++ questão3.c
@@ -4,14 +4,14 @@
 float notas[];
 
 float aritimetico (){
+    
     int qtd_notas;
 
     printf("Digite a quantidade de notas a serem calculadas: ");
     scanf("%d", &qtd_notas);
     fflush(stdin);
     
-    for (int i = 0; i < qtd_notas; i++)
-    {
+    for (int i = 0; i < qtd_notas; i++){
         printf("Digite a nota %d: ", i + 1);
         scanf("%f", &notas[i]);
         fflush(stdin);
@@ -19,16 +19,29 @@ float aritimetico (){
     
     float soma;
 
-    for (int i = 0; i < qtd_notas; i++)
-    {
+    for (int i = 0; i < qtd_notas; i++){
         soma = soma + notas[i];
     }
     
     float media = soma / (float)qtd_notas;
 
-    printf("soma %.2f\nmedia %.2f\n", soma, media);
+    for (int i = 0; i < qtd_notas; i++){
+        if (notas[i] > 10 || notas[i] < 0){
+            printf("Nota %d invalida!\n", i + 1);
+        }        
+    }
+
+    for (int i = 0; i < qtd_notas; i++){
+        if (notas[i] > 10 || notas[i] < 0){
+            return 0;
+        }else{
+            return media; 
+        }
+    }
+}
+
+float ponderado(){
     
-    return media;
 }
 
 int main()
@@ -39,13 +52,19 @@ int main()
     scanf("%d", &opcao);
     fflush(stdin);
 
-    if (opcao == 1)
-    {
+    if (opcao == 1){
+        
         float mediaA = aritimetico();
 
-        printf("Sua media aritimetica: %.2f", mediaA);
+        if (mediaA == 0){
+            printf("Tente novamente!");
+        }else{
+            printf("Sua media aritimetica: %.2f", mediaA);
+        }
+    }else if (opcao == 2){
+        
     }
     
-
+    
     return 0;
 }
